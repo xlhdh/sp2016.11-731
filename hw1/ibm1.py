d813dc790079cb5e993c992c3525db515cc8a94a
Yizhou Hu <yizhouh+github@andrew.cmu.edu>
2016-02-09 02:26:08 -0500

    new ibm with german break

--- hw1/ibm1.py
+++ hw1/ibm1.py
@@ -1,8 +1,92 @@
+# -*- coding: UTF-8 -*-
 #!/usr/bin/env python
 import optparse
 import sys
 from collections import defaultdict
 import numpy as np
+
+def prep(bitext):
+  engcorp, fracorp, engnum, franum = [], [], [], []
+  fradict = {}
+  with open("gdrdict.txt") as fl: 
+    for ln in fl: 
+      ln = ln[:-1].split('=')
+      fradict[ln[0]]=ln[1].split('+')
+  for (sf, se) in bitext: 
+    # ENG side
+    engcorp.append(se+["_NULL_",])
+    engnum.append(range(len(se))+[None,])
+
+    # FRA side 
+    fline = []
+    fnum = []     
+    for (i, fword) in enumerate(sf): 
+      fword = fword.strip()
+      if any(c.isdigit() for c in fword): 
+        fword = [fword, ]
+      else: 
+        fword = fword.replace("„","").split("-")
+      fout = []
+      for f in fword: 
+        if f in fradict: 
+          fout.extend(fradict[f])
+        else: 
+          fout.extend([f,])
+      fline.extend(fout)
+      fnum.extend([i,]*len(fout))
+
+    try: 
+      while True: 
+        i = fline.index("")
+        del fline[i]
+        del fnum[i]
+    except ValueError: 
+      pass
+    assert len(fline)==len(fnum)
+    fracorp.append(fline+["_NULL_",])
+    franum.append(fnum+[None,])
+
+  assert len(engcorp)== len(fracorp)== len(engnum)== len(franum)
+  return engcorp, fracorp, engnum, franum 
+
+def e2f(engcorp, fracorp):
+  t = defaultdict(lambda: defaultdict(lambda: float(1)))
+  for itr in range(6):
+    sys.stderr.write("itr: "+str(itr)+"\n")
+    count = defaultdict(lambda: defaultdict(float))
+    total = defaultdict(float)
+    for itr in range(len(engcorp)):
+      se = engcorp[itr]
+      sf = fracorp[itr]
+      s = defaultdict(float)
+      for e in se:
+        for f in sf: 
+          s[e] += t[e][f]
+      for e in se: 
+        for f in sf: 
+          count[e][f] += t[e][f]/s[e]
+          total[f] += t[e][f]/s[e]
+    
+    for f in total.keys():
+      for e in t.keys():
+        if f in t[e]:
+          t[e][f] = count[e][f]/total[f]
+  return t
+
+def dcd(engcorp, fracorp, engnum, franum, tef):
+  opt  = []
+  for itr in range(len(engcorp)):
+    optline = []
+    se = engcorp[itr]
+    sf = fracorp[itr]
+    for (i, e) in enumerate(se): 
+      x = franum[itr][np.argmax([tef[e][f] for f in sf])], engnum[itr][i]
+      if x[0] and x[1]:
+        optline.append(x)
+    opt.append(optline)
+  return opt
+
+
 
 optparser = optparse.OptionParser()
 optparser.add_option("-b", "--bitext", dest="bitext", default="data/dev-test-train.de-en", help="Parallel corpus (default data/dev-test-train.de-en)")
@@ -10,65 +94,22 @@ optparser.add_option("-n", "--num_sentences", dest="num_sents", default=sys.maxi
 (opts, _) = optparser.parse_args()
 
 sys.stderr.write("Training with IBM1...\n")
-bitext = [[sentence.strip().split() for sentence in pair.split(' ||| ')] for pair in open(opts.bitext)][:opts.num_sents]
+bitext = [[sentence.strip().lower().split() for sentence in pair.split(' ||| ')] for pair in open(opts.bitext)][:opts.num_sents]
+sys.stderr.write("Preparing...\n")
+engcorp, fracorp, engnum, franum = prep(bitext)
 
-'''
-for (e, f) in bitext: 
-  e = e.extend("_NULL_")
-  f = f.extend("_NULL_")
-'''
+tef = e2f(engcorp, fracorp)
+tfe = e2f(fracorp, engcorp)
 
-def e2f(thebitext):
-  t = defaultdict(lambda: defaultdict(lambda: float(1)))
-  for itr in range(6):
-    sys.stderr.write("itr: "+str(itr)+"\n")
-    count = defaultdict(lambda: defaultdict(float))
-    total = defaultdict(float)
-    for (sf, se) in thebitext:
-      s = defaultdict(float)
-      for e in se:
-        for f in sf: 
-          s[e] += t[e][f]
-      for e in se: 
-        for f in sf: 
-          count[e][f] += t[e][f]/s[e]
-          total[f] += t[e][f]/s[e]
-    
-    for f in total.keys():
-      for e in t.keys():
-        if f in t[e]:
-          t[e][f] = count[e][f]/total[f]
-  return t
-def f2e(thebitext):
-  t = defaultdict(lambda: defaultdict(lambda: float(1)))
-  for itr in range(6):
-    sys.stderr.write("itr: "+str(itr)+"\n")
-    count = defaultdict(lambda: defaultdict(float))
-    total = defaultdict(float)
-    for (se, sf) in thebitext:
-      s = defaultdict(float)
-      for e in se:
-        for f in sf: 
-          s[e] += t[e][f]
-      for e in se: 
-        for f in sf: 
-          count[e][f] += t[e][f]/s[e]
-          total[f] += t[e][f]/s[e]
-    
-    for f in total.keys():
-      for e in t.keys():
-        if f in t[e]:
-          t[e][f] = count[e][f]/total[f]
-  return t
+todecode = 150
+dcdef = dcd(engcorp[:todecode], fracorp[:todecode], engnum[:todecode], franum[:todecode], tef)
+dcdfe = dcd(fracorp[:todecode], engcorp[:todecode], franum[:todecode], engnum[:todecode], tfe)
+dcdfe = [[(y, x) for x, y in l] for l in dcdfe]
 
+alm  = [set(x).union(set(y)) for x, y in zip(dcdef, dcdfe)]
+#alm  = [set(x).intersection(set(y)) for x, y in zip(dcdef, dcdfe)]
 
-tef = e2f(bitext)
-tfe = f2e(bitext)
+for i in range(len(dcdef)): 
+  sys.stdout.write(' '.join([str(x[0])+'-'+str(x[1]) for x in alm[i]])+'\n')
 
-for (sf, se) in bitext[:150]:
-  for (i, e) in enumerate(se): 
-      sys.stdout.write(str(np.argmax([tef[e][f] for f in sf]))+"-"+str(i)+" ")
-  for (i, f) in enumerate(sf): 
-      sys.stdout.write(str(i)+"-"+str(np.argmax([tfe[f][e] for e in se]))+" ")
-  sys.stdout.write("\n")
 
