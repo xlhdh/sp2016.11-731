8b1774e7bb19846cb64d0e314bcbe15b0b4cd198
Yizhou Hu <yizhouh+github@andrew.cmu.edu>
2016-03-31 19:46:25 -0400

    hardcore jobs

---
+++ hw3/hardcore.sh
@@ -0,0 +1,8 @@
+nohup ./aproto-sum -s 300 -x 2 & 
+nohup ./aproto-sum -s 300 -x 9 & 
+nohup ./aproto-sum -s 300 -x 10 & 
+nohup ./aproto-sum -s 300 -x 19 & 
+nohup ./aproto-sum -s 300 -x 23 & 
+nohup ./aproto-sum -s 300 -x 33 & 
+nohup ./aproto-sum -s 300 -x 39 & 
+nohup ./aproto-sum -s 300 -x 54 & 
