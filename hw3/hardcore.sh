fb37d41a6ede031abe0bc55cabaa590cd028916d
Yizhou Hu <yizhouh+github@andrew.cmu.edu>
2016-04-01 01:48:51 -0400

    lmbug1

--- hw3/hardcore.sh
+++ hw3/hardcore.sh
@@ -1,8 +1,8 @@
-nohup ./aproto-sum -s 300 -x 2 & 
-nohup ./aproto-sum -s 300 -x 9 & 
-nohup ./aproto-sum -s 300 -x 10 & 
-nohup ./aproto-sum -s 300 -x 19 & 
-nohup ./aproto-sum -s 300 -x 23 & 
-nohup ./aproto-sum -s 300 -x 33 & 
-nohup ./aproto-sum -s 300 -x 39 & 
-nohup ./aproto-sum -s 300 -x 54 & 
+nohup ./$0 -s $1 -x 2 & 
+nohup ./$0 -s $1 -x 9 & 
+nohup ./$0 -s $1 -x 10 & 
+nohup ./$0 -s $1 -x 19 & 
+nohup ./$0 -s $1 -x 24 & 
+nohup ./$0 -s $1 -x 33 & 
+nohup ./$0 -s $1 -x 39 & 
+nohup ./$0 -s $1 -x 54 & 
