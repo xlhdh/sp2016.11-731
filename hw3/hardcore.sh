037eaa591f4ed6f6b7759865a594179bc5af5e3d
Yizhou Hu <yizhouh+github@andrew.cmu.edu>
2016-04-01 01:53:43 -0400

    hardcorebug

--- hw3/hardcore.sh
+++ hw3/hardcore.sh
@@ -1,8 +1,8 @@
-nohup ./$0 -s $1 -x 2 & 
-nohup ./$0 -s $1 -x 9 & 
-nohup ./$0 -s $1 -x 10 & 
-nohup ./$0 -s $1 -x 19 & 
-nohup ./$0 -s $1 -x 24 & 
-nohup ./$0 -s $1 -x 33 & 
-nohup ./$0 -s $1 -x 39 & 
-nohup ./$0 -s $1 -x 54 & 
+nohup ./$1 -s $2 -x 2 & 
+nohup ./$1 -s $2 -x 9 & 
+nohup ./$1 -s $2 -x 10 & 
+nohup ./$1 -s $2 -x 19 & 
+nohup ./$1 -s $2 -x 24 & 
+nohup ./$1 -s $2 -x 33 & 
+nohup ./$1 -s $2 -x 39 & 
+nohup ./$1 -s $2 -x 54 & 
